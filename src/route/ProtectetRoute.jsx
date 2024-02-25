83b466fa3877991a77cd86a2692846635529a34e
roid zuhdianto <roidzuh@gmail.com>
2024-02-25 12:42:20 +0700

    update Protected Route

--- src/route/ProtectetRoute.jsx
+++ src/route/ProtectetRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("access_token");
   if (!token) {
-    return <Navigate to="/" />;
+    return <Navigate to="/login" />;
   }
 
   return <>{children || <Outlet />}</>;
